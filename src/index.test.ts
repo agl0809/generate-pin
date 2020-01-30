5cb0c932f755c1bb9e81b5e518f77639b9c68524
antonio.garcia <antonio.garcia@sngular.com>
2020-01-29 21:00:26 -0500

    feat(index): return multiple pins by sending a size

--- src/index.test.ts
+++ src/index.test.ts
@@ -3,3 +3,9 @@ import { generatePin } from './index';
 test('should return one-pin list by default', () => {
   expect(generatePin().length).toEqual(1);
 });
+
+test('should return a list of pins with a certain size', () => {
+  const size = 5;
+
+  expect(generatePin(size).length).toEqual(size);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-export const generatePin = (size?: number): string[] => [Math.random().toString()];
+export const generatePin = (size: number = 1): string[] => {
+  const pinCollection: string[] = [];
+
+  while (pinCollection.length < size) {
+    const newPin = Math.random().toString();
+    pinCollection.push(newPin);
+  }
+
+  return pinCollection;
+};
