b6c63e54f596785318f8fa8460794ab5dd92e9f1
Antonio Garcia <agl0809@gmail.com>
2020-01-30 23:08:00 -0500

    feat(index): discart three consecutive digits ordered pins

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,48 +1,50 @@
 import { getPins } from '../index';
 
 describe('generate-pin', () => {
-  describe('generating a collection', () => {
-    test('should return one pin by default', () => {
-      expect(getPins().length).toEqual(1);
-    });
+  test('should return one pin by default', () => {
+    expect(getPins().length).toEqual(1);
+  });
 
-    test('should return a certain number of pins', () => {
-      const size = 5;
+  test('should return a certain number of pins', () => {
+    const size = 5;
 
-      expect(getPins(size).length).toEqual(size);
+    expect(getPins(size).length).toEqual(size);
+  });
+
+  test('should be strings', () => {
+    const pins = getPins();
+
+    pins.forEach((pin: string) => {
+      expect(typeof pin).toEqual('string');
     });
   });
 
-  describe('generating pins', () => {
-    test('should be strings', () => {
-      const pins = getPins();
+  test('should have four digits', () => {
+    const pins = getPins();
 
-      pins.forEach((pin: string) => {
-        expect(typeof pin).toEqual('string');
-      });
+    pins.forEach((pin: string) => {
+      expect(pin.length).toEqual(4);
     });
+  });
 
-    test('should have four digits', () => {
-      const pins = getPins();
+  test('should not have two same consecutive digits', () => {
+    const pins = getPins();
 
-      pins.forEach((pin: string) => {
-        expect(pin.length).toEqual(4);
-      });
+    pins.forEach((pin: string) => {
+      const digits = pinToDigits(pin);
+      expect(hasSameNeigbors(digits)).toBeFalsy();
     });
+  });
 
-    test('should not have two same consecutive digits', () => {
-      const pins = getPins();
+  test('should not have three consecutive digits asc or desc', () => {
+    const pins = getPins();
 
-      pins.forEach((pin: string) => {
-        const digits = pinToDigits(pin);
-        expect(hasSameNeigbors(digits)).toBeFalsy();
-      });
+    pins.forEach((pin: string) => {
+      const digits = pinToDigits(pin);
+      expect(hasThreeDigitsOrdered(digits)).toBeFalsy();
     });
   });
 });
-
-const pinToDigits = (pin: string): number[] =>
-  pin.split('').map((elem: string) => parseInt(elem, 10));
 
 const hasSameNeigbors = (pin: number[]): boolean => {
   for (let i = 0; i < pin.length - 1; i++) {
@@ -50,6 +52,14 @@ const hasSameNeigbors = (pin: number[]): boolean => {
       return true;
     }
   }
-
   return false;
 };
+
+const pinToDigits = (pin: string): number[] =>
+  pin.split('').map((elem: string) => parseInt(elem, 10));
+
+const hasThreeDigitsOrdered = (pin: number[]): boolean =>
+  isOrdered(pin.slice(0, 3)) || isOrdered(pin.slice(1));
+
+const isOrdered = (segment: number[]): boolean =>
+  segment[1] * 3 === segment[0] + segment[1] + segment[2] ? true : false;
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,11 @@
-const getRandomPin = (): string => {
-  const randomPin = Math.random()
-    .toFixed(4)
-    .toString();
-
-  return randomPin.substring(randomPin.indexOf('.') + 1);
-};
-
 const pinToDigits = (pin: string): number[] =>
   pin.split('').map((elem: string) => parseInt(elem, 10));
+
+const isOrdered = (segment: number[]): boolean =>
+  segment[1] * 3 === segment[0] + segment[1] + segment[2] ? true : false;
+
+const hasThreeDigitsOrdered = (pin: number[]): boolean =>
+  isOrdered(pin.slice(0, 3)) || isOrdered(pin.slice(1));
 
 const hasSameNeigbors = (pin: number[]): boolean => {
   for (let i = 0; i < pin.length - 1; i++) {
@@ -18,11 +16,21 @@ const hasSameNeigbors = (pin: number[]): boolean => {
   return false;
 };
 
+const getRandomPin = (): string => {
+  const randomPin = Math.random()
+    .toFixed(4)
+    .toString();
+
+  return randomPin.substring(randomPin.indexOf('.') + 1);
+};
+
 const getValidPin = (): string => {
   const pin = getRandomPin();
   const digits = pinToDigits(pin);
 
-  return hasSameNeigbors(digits) ? getValidPin() : digits.join('');
+  return hasSameNeigbors(digits) || hasThreeDigitsOrdered(digits)
+    ? getValidPin()
+    : digits.join('');
 };
 
 export const getPins = (size: number = 1): string[] => {
